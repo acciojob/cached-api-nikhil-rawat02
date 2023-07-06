884fd4be86b223a80bc4747076506748fd3e3e32
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-07-06 10:41:06 +0530

    solution1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [result, setResult] = useState([]);
+  const [inputData, setInputData] = useState("");
+  const [error, setError] = useState("");
+  const [showData, setShowData] = useState([]);
+  const handleInput = (e) =>{
+    setInputData(e.target.value);
+    setShowData({});
+
+    const show = result.map(item =>{
+      console.log(item);
+    })
+
+  }
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/')
+    .then( data => data.json().then((dataSet) => {
+      setError("");
+      setResult(data);
+      console.log(data);
+    })
+    )
+    .catch((error)=>{
+      setError(error.message);
+    })
+  },[])
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" value={inputData} onChange={handleInput}/>
+        <div style={{margin:"10px"}}>
+          
+        </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { useState, useEffect, useMemo } from "react";
+import './../styles/App.css';
+import { fetchData } from "./api";
+import 'regenerator-runtime/runtime'
 
+function App({ id }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
 
+  const cachedData = useMemo(() => {
+    return fetchData(id);
+  }, [id]);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  useEffect(() => {
+    setIsLoading(true);
+    setData(null);
+
+    const fetchDataAsync = async () => {
+      const result = await cachedData;
+      setData(result);
+      setIsLoading(false);
+    };
+
+    fetchDataAsync();
+  }, [id, cachedData]);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <ul>
+            {data.map((data) => (
+              <li key={data.id}>
+                <h4>{data.title}</h4>
+                <p>{data.body}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
